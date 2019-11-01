875142eb3dec758cd3a54c0301ddff58d19a5309
HirudineeADRT <hirudinee@adroitlogic.com>
2019-11-01 13:32:45 +0530

    translate

--- hiru1101_translater/lambda.js
+++ hiru1101_translater/lambda.js
@@ -1,17 +1,50 @@
 let AWS = require('aws-sdk');
+const s3 = new AWS.S3();
 const translate = new AWS.Translate();
 
 exports.handler = function (event, context, callback) {
+    // translate.listTerminologies({
+    //     MaxResults: 5,
+    //     NextToken: "this is a test"
+    // }).promise()
+    //     .then(data => {
+    //         console.log(data)
+    //     })
+    //     .catch(err => {
+    //         console.log(err)// error handling goes here
+    //     });
+
+
+
+
     translate.translateText({
         SourceLanguageCode: "en",
-        TargetLanguageCode: "ko",
-        Text: "Hello world"
+        TargetLanguageCode: "nl",
+        Text: "om tum, or spicy green papaya salad, comes from Thailand’s north-eastern state of Isaan. Garlic, chillies, green beans, cherry tomatoes, and shredded raw papaya are pounded in a pestle and mortar, releasing a sweet-sour-spicy flavour that's quite distinctive. Regional variations include peanuts, dry shrimp or salted crab into the mix. This dish can be polarizing, some can't get enough of its taste, while others simply can't handle the spice."
     }).promise()
         .then(data => {
-            // your code goes here
+            console.log(data)
         })
         .catch(err => {
-            // error handling goes here
+            console.log(err)
+        });
+    s3.putObject({
+        "Body": "gg",
+        "Bucket": "as2-test-lahiru",
+        "Key": "gg"
+    })
+        .promise()
+        .then(data => {
+            console.log(data);           // successful response
+            /*
+            data = {
+                ETag: "\"6805f2cfc46c0f04559748bb039d69ae\"",
+                VersionId: "pSKidl4pHBiNwukdbcPXAIs.sshFFOc0"
+            }
+            */
+        })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
         });
 
     callback(null, { "message": "Successfully executed" });
---
+++ hiru1101_translater/newpy.py
@@ -0,0 +1,55 @@
+import boto3
+s3 = boto3.client("s3")
+
+def handler(event, context):
+
+    try:
+        data = s3.copy_object(
+            Bucket="cloud9-ktest",
+            CopySource="/thisisanewbucket/hh",
+            Key="hh"
+        )
+    
+        """
+        data = {
+            "CopyObjectResult": {
+                "ETag": "\"6805f2cfc46c0f04559748bb039d69ae\"",
+                "LastModified": "<Date Representation>"
+            }
+        }
+        """
+    except BaseException as e:
+        print(e)
+
+
+
+
+    try:
+        data = s3.list_objects(
+            Bucket='as2-test-lahiru',
+            MaxKeys=10,
+            Prefix=''
+        )
+    
+        """
+        data = {
+            "Contents": [
+               {
+                  "ETag": "\"70ee1738b6b21e2c8a43f3a5ab0eee71\"",
+                  "Key": "example1.jpg",
+                  "LastModified": "<Date Representation>",
+                  "Owner": {
+                     "DisplayName": "myname",
+                     "ID": "12345example25102679df27bb0ae12b3f85be6f290b936c4393484be31bebcc"
+                  },
+                  "Size": 11,
+                  "StorageClass": "STANDARD"
+               },
+               # {...}
+           ]
+        }
+        """
+    except BaseException as e:
+        print(e)
+    
+    return {"message": "Successfully executed"}
