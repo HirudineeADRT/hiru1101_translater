7797923f0ab88a5e947700f44a3d7f3fde655bf2
HirudineeADRT <hirudinee@adroitlogic.com>
2019-11-01 17:29:18 +0530

    test

--- hiru1101_translater/lambda.js
+++ hiru1101_translater/lambda.js
@@ -3,32 +3,6 @@ const s3 = new AWS.S3();
 const translate = new AWS.Translate();
 
 exports.handler = function (event, context, callback) {
-    // translate.listTerminologies({
-    //     MaxResults: 5,
-    //     NextToken: "this is a test"
-    // }).promise()
-    //     .then(data => {
-    //         console.log(data)
-    //     })
-    //     .catch(err => {
-    //         console.log(err)// error handling goes here
-    //     });
-
-
-
-
-    translate.translateText({
-        SourceLanguageCode: "en",
-        TargetLanguageCode: "nl",
-        Text: "om tum, or spicy green papaya salad, comes from Thailand’s north-eastern state of Isaan. Garlic, chillies, green beans, cherry tomatoes, and shredded raw papaya are pounded in a pestle and mortar, releasing a sweet-sour-spicy flavour that's quite distinctive. Regional variations include peanuts, dry shrimp or salted crab into the mix. This dish can be polarizing, some can't get enough of its taste, while others simply can't handle the spice."
-    }).promise()
-        .then(data => {
-            console.log(data)
-        })
-        .catch(err => {
-            console.log(err)
-        });
-
 
     callback(null, { "message": "Successfully executed" });
 }
--- hiru1101_translater/newpy.py
+++ hiru1101_translater/newpy.py
@@ -3,5 +3,4 @@ s3 = boto3.client("s3")
 
 def handler(event, context):
 
-    
     return {"message": "Successfully executed"}
